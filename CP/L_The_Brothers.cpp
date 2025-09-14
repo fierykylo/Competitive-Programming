560cd56db52b8ac863eef823aafa060336fca2a9
Aarush Dubey <mrknownunknown2702@gmail.com>
2025-09-14 23:55:40 +0530

    Add L_The_Brothers.cpp

---
+++ CP/L_The_Brothers.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(false);
+   
+    string f1 , f2  , s1 , s2;
+    cin >> f1 >> s1;
+    cin >> f2 >> s2;
+    if(s1 == s2)
+        {
+            cout << "ARE Brothers" << "\n";
+
+        }
+    else
+        {
+            cout << "NOT" << "\n";
+        }
+}
